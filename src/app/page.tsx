dfe011b6a549b24caafbb2c5862abd27efe2e386
najdek <mateusz@najdek.me>
2024-03-23 02:56:51 +0100

    Move skills into component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { AllSkills } from "@/components/skills";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -55,75 +56,7 @@ export default function Home() {
               <div className="text-3xl text-gray-900">About Me</div>
               <div className="text-l mt-4 text-gray-900 bg-sky-50 p-6 rounded-lg drop-shadow-md max-w-sm sm:max-w-2xl">Since childhood, I've been fascinated by technology and programming. It's a passion that's stuck with me through the years and continues to drive me forward. With a natural curiosity and a love for problem-solving, I'm always eager to explore new developments in the tech world and apply my skills to create innovative solutions.</div>
             </div>
-
-            <div className="flex flex-col mx-auto text-center relative mt-16">
-              <h1 className="text-3xl text-gray-900">Skills</h1>
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 p-6 container">
-
-                <div className="bg-sky-50 py-2 px-4 rounded-lg drop-shadow-md max-w-lg m-2">
-                  <span className="font-semibold">Frontend Development:</span> Proficient in HTML, CSS, and JavaScript, with experience in modern frameworks such as React/Next.js.
-                  Skilled in crafting responsive and visually appealing web interfaces.
-                </div>
-
-
-                <div className="bg-sky-50 p-6 rounded-lg drop-shadow-md max-w-lg m-2">
-                  descriptiontdrtrd
-                </div>
-                <div className="bg-sky-50 p-6 rounded-lg drop-shadow-md max-w-lg m-2">
-                  descriptiontdrtrd
-                </div>
-                <div className="bg-sky-50 p-6 rounded-lg drop-shadow-md max-w-lg m-2">
-                  descriptiontdrtrd
-                </div>
-                <div className="bg-sky-50 p-6 rounded-lg drop-shadow-md max-w-lg m-2">
-                  descriptiontdrtrd
-                </div>
-
-
-              </div>
-
-            </div>
-
-
-            <div className="container grid grid-cols-1 md:grid-cols-2 p-6">
-
-
-              <div className="bg-sky-50 p-6 rounded-lg drop-shadow-md m-2">
-                <div className="text-xl mb-2">
-                  About Me
-                </div>
-                <div className="text-l leading-relaxed">
-                  Since childhood, I've been fascinated by technology and programming.
-                  It's a passion that's stuck with me through the years and continues to drive me forward.
-                  With a natural curiosity and a love for problem-solving, I'm always eager to explore new developments in the tech world and apply my skills to create innovative solutions.
-                </div>
-              </div>
-
-              <div className="bg-sky-50 p-6 rounded-lg drop-shadow-md m-2 leading-relaxed">
-                <div className="text-xl mb-2">
-                  Skills
-                </div>
-                <div className="text-l">
-                  <span className="font-semibold">Frontend Development:</span> Proficient in HTML, CSS, and JavaScript, with experience in modern frameworks such as React/Next.js.
-                  Skilled in crafting responsive and visually appealing web interfaces.
-                  <br />
-                  <span className="font-semibold">PHP:</span> Familiar with PHP for backend development, capable of implementing basic functionalities and integrating with frontend technologies.
-                  <br />
-                  <span className="font-semibold">MySQL:</span> Experience with MySQL for database management in web development projects, with a basic understanding of database design and querying.
-                  <br />
-                  <span className="font-semibold">VB.NET:</span> Familiarity with VB.NET for developing Windows applications, primarily utilized in my business project for graphic design management.
-                  <br />
-                  <span className="font-semibold">Git:</span> Over 10 years of experience using Git. Initially used GitHub for storing device repositories for custom Android ROM compilation. Now storing majority of my code on it.
-                  <br />
-                  <span className="font-semibold">REST API:</span> Utilized Allegro's REST API to manage orders and automate processes in my business.
-                  <br />
-                  <span className="font-semibold">Docker:</span> Got familiar with Docker by hosting various services on my home media server, although haven't used it in development yet.
-                  <br />
-                </div>
-              </div>
-
-            </div>
-
+<AllSkills />
           </div>
         </section>
       </div>
