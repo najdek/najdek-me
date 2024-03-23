c3a8b62a76b0d44eb92a0d3114026c4948011acf
najdek <mateusz@najdek.me>
2024-03-23 14:13:12 +0100

    Skills: change layout - make place for icons

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -2,8 +2,13 @@ import { skillList } from "@/app/skills";
 function Skill(props: any) {
 
     return (
-        <div className="bg-sky-50 py-2 px-4 rounded-lg drop-shadow-md max-w-lg sm:max-w-xs m-2">
-            <span className="font-semibold">{props.name}</span><br />{props.desc}</div>
+        <div className="flex bg-sky-50 py-2 px-4 rounded-lg drop-shadow-md max-w-sm m-2 space-x-4">
+            <div className="min-w-[30%] flex flex-col justify-center">
+                <div className="bg-blue-200 w-full h-24">icon</div>
+                <div className="font-semibold">{props.name}</div>
+            </div>
+            <div className="flex items-center">{props.desc}</div>
+        </div>
     )
 }
 
