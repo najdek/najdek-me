94cb2eae97547b28a44091fbf4fb07f4a57f8ac2
najdek <mateusz@najdek.me>
2024-03-24 01:43:22 +0100

    Preserve page scroll when returning from project page

--- src/app/project/[id]/page.tsx
+++ src/app/project/[id]/page.tsx
@@ -1,16 +1,17 @@
 "use client";
 import { NavbarFixed } from "@/components/navbar";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function ProjectPage({ params }: { params: { id: string } }) {
   const projectId = params.id;
+  const router = useRouter();
 
   return (
     <div>
       <NavbarFixed />
       <section className="flex justify-between flex-col min-h-screen relative bg-gradient-to-b from-sky-100 to-sky-300">
         <div className="flex flex-col mx-auto mt-16 min-h-screen">
-          <Link href="../">{`<- Go Back`}</Link>
+          <div onClick={() => router.back()} className="cursor-pointer">{`<- Go Back`}</div>
           {projectId}
         </div>
       </section>
