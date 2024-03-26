99147fe5df4189b47e1f72ba0180830467269c30
najdek <mateusz@najdek.me>
2024-03-26 11:16:22 +0100

    Added favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-       <meta name="darkreader-lock" />
+        <meta name="darkreader-lock" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        <link rel="manifest" href="/icons/site.webmanifest" />
+        <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="shortcut icon" href="/icons/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#2d89ef" />
+        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+        <meta name="theme-color" content="#ffffff" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
