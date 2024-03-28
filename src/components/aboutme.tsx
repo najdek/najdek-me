b8c86fdc580991618c629cebb060102bb82d6cfe
najdek <mateusz@najdek.me>
2024-03-28 14:03:54 +0100

    Aboutme: added small padding

--- src/components/aboutme.tsx
+++ src/components/aboutme.tsx
@@ -2,7 +2,7 @@ import { aboutMe } from "@/data/aboutme"
 
 export function AboutMe() {
     return (
-        <div className="flex flex-col mx-auto text-center relative">
+        <div className="flex flex-col mx-auto px-4 text-center relative">
             <div className="text-3xl text-gray-900 dark:dark:text-gray-200">About Me</div>
             <div className="text-md mt-6 text-gray-900 bg-sky-50 dark:bg-cyan-950 dark:text-gray-300 p-6 rounded-lg drop-shadow-md max-w-sm sm:max-w-2xl">{aboutMe.text}</div>
         </div>
